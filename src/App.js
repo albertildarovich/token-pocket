0e8f16129065c5f4587b102d98f7c4af60aeb045
albert.kadyrov <albert.kadyrov@haiku.dev>
2021-07-19 19:42:23 +0300

    add tp js sdk

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import tp from 'tp-js-sdk';
+
 function App() {
+  const [appInfo, setAppInfo] = useState('no info');
+  useEffect(() => {
+    getAppInfo();
+  }, []);
+  const getAppInfo = async () => {
+    console.log('Getting app info');
+    const appInfo = await tp.getAppInfo();
+    setAppInfo(appInfo);
+    console.log('AppInfo: ', appInfo);
+  }
+  tp.importWallet('eth');
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {appInfo}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
